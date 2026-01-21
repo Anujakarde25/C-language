5ed18f2011904e6e276a11dc8f0c31cb060ca9ad
Anujakarde25 <anujakarde6256@gmail.com>
2026-01-21 11:36:52 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int i,num;
+	printf("enter number:");
+	scanf("%d",&num);
+	for(i=2; i<num; i++)
+	{
+		if(num%i==0)
+		{
+			break;
+		}
+	}
+	if(i==num)
+	printf("number is prime");
+	else
+	printf("number is not prime");
+}
