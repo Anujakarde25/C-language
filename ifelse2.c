f05c2142289b9e73f009c1a70aa7daf3f1b1af0e
Anujakarde25 <anujakarde6256@gmail.com>
2026-01-25 22:30:29 +0530

    Add files via upload

---
+++ ifelse2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	printf("enter age:");
+	scanf("%d",&age);
+	if(age>=18)
+	printf("eligible of voting");
+	else
+	printf("not eligible for voting");
+}
---
+++ ifelse3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter value of a:");
+	scanf("%d",&a);
+	printf("enter value of b:");
+	scanf("%d",&b);
+	if(a>b)
+	printf("a is greater than b");
+	else
+	printf("b is greater than a");	
+}
