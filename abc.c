a8da2525b4dc8324276fc5c9c741b8c7020aabd5
Anujakarde25 <anujakarde6256@gmail.com>
2026-01-25 22:55:41 +0530

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter value of a:");
+	scanf("%d",&a);
+	printf("enter value of b:");
+	scanf("%d",&b);
+	printf("enter value of c:");
+	scanf("%d",&c);
+	if(a>b && a>c)
+	printf("a is greater ");
+	else if(b>a && b>c)
+	printf("b is greater");	
+	else
+	printf("c is greater");
+}
