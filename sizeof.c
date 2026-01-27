e1c29aa5548ac09f03bcb5a1620f7b913cd75b8e
Anujakarde25 <anujakarde6256@gmail.com>
2026-01-27 22:21:36 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	printf("char=%d",sizeof(char));
+	printf("\nfloat=%d",sizeof(float));
+	printf("\nint=%d",sizeof(int));
+	printf("\ndouble=%d",sizeof(double));	
+}
